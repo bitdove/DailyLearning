19e67cef31dec69b7c7ff182b5980eb5afc9c6ad
mrxuxg <mrxuxg@gmail.com>
2021-07-22 10:29:27 +0800

    update mergesort

--- A02_数据结构与算法/MergeSort.cpp
+++ A02_数据结构与算法/MergeSort.cpp
@@ -6,12 +6,26 @@ std::vector<int> temp;
 void MergeSort(std::vector<int> &arr, int left, int right){
     if(left >= right)
         return;
-    int mid = left + (right - left) >> 1;
+    int mid = left + ((right - left) >> 1);
 
     MergeSort(arr, left, mid);
     MergeSort(arr, mid+1, right);
 
-    int 
+    int i = left;
+    int j = mid + 1;
+    int count = left;
+    while(i <= mid && j <= right){
+        if(arr[i] <= arr[j])
+            temp[count++] = arr[i++];
+        else
+            temp[count++] = arr[j++];
+    }
+    while(i <= mid)
+        temp[count++] = arr[i++];
+    while(j <= right)
+        temp[count++] = arr[j++];
+    for(int k = left; k <= right; ++k)
+        arr[k] = temp[k];
 }
 
 int main(){
